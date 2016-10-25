7e89964dbd4679326b41790b8069ab8bd237814b
Clyde Grey <clyde.grey@gmail.com>
2016-10-25 12:47:51 -0400

    Initial Commit

---
+++ dev/scripts/main.js
@@ -0,0 +1,7 @@
+const ohHey = "Hello World";
+
+const numbers = [1,2,3,6,9,99];
+
+
+
+console.log(ohHey);
---
+++ public/scripts/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var ohHey = "Hello World";
+
+var numbers = [1, 2, 3, 6, 9, 99];
+
+console.log(ohHey);
